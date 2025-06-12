6ac501fce188de392944dacff5eb4f5a35bf79ea
Carlos Henrique Rafaeli <gordinez658@gmail.com>
2025-06-12 16:58:02 -0300

    Implementado modulo conta

---
+++ ControleDeBar.Dominio/ModuloConta/Pedido.cs
@@ -0,0 +1,29 @@
+﻿using ControleDeBar.Dominio.ModuloProduto;
+
+namespace ControleDeBar.Dominio.ModuloConta;
+
+public class Pedido
+{
+    public Guid Id { get; set; }
+    public Produto Produto { get; set; }
+    public int Quantidade { get; set; }
+
+    public Pedido() { }
+
+    public Pedido(Produto produto, int quantidade) : this()
+    {
+        Id = Guid.NewGuid();
+        Produto = produto;
+        Quantidade = quantidade;
+    }
+
+    public decimal CalcularTotalParcial()
+    {
+        return Produto.Valor * Quantidade;
+    }
+
+    public override string ToString()
+    {
+        return $"{Quantidade}x {Produto}";
+    }
+}
---
+++ ControleDeBar.Infraestrutura.Arquivos/ModuloConta/RepositorioContaEmArquivo.cs
@@ -0,0 +1,65 @@
+﻿using ControleDeBar.Dominio.ModuloConta;
+using ControleDeBar.Infraestrura.Arquivos.Compartilhado;
+
+namespace ControleDeBar.Infraestrutura.Arquivos.ModuloContal;
+
+public class RepositorioContaEmArquivo : IRepositorioConta
+{
+    private readonly ContextoDados contexto;
+    protected readonly List<Conta> registros;
+
+    public RepositorioContaEmArquivo(ContextoDados contexto)
+    {
+        this.contexto = contexto;
+        registros = contexto.Contas;
+    }
+
+    public void CadastrarConta(Conta conta)
+    {
+        registros.Add(conta);
+
+        contexto.Salvar();
+    }
+
+    public List<Conta> SelecionarContas()
+    {
+        return registros;
+    }
+
+    public List<Conta> SelecionarContasAbertas()
+    {
+        var contasAbertas = new List<Conta>();
+
+        foreach (var item in registros)
+        {
+            if (item.EstaAberta)
+                contasAbertas.Add(item);
+        }
+
+        return contasAbertas;
+    }
+
+    public List<Conta> SelecionarContasFechadas()
+    {
+        var contasFechadas = new List<Conta>();
+
+        foreach (var item in registros)
+        {
+            if (!item.EstaAberta)
+                contasFechadas.Add(item);
+        }
+
+        return contasFechadas;
+    }
+
+    public Conta SelecionarPorId(Guid idRegistro)
+    {
+        foreach (var item in registros)
+        {
+            if (item.Id == idRegistro)
+                return item;
+        }
+
+        return null;
+    }
+}
